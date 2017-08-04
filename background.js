37bd7560ada8e3e0ebb2c3ab806b0d496292ea74
Simmo Saan <simmo.saan@gmail.com>
2017-08-04 13:09:56 +0300

    Filter youtube.com while listening history API

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
 browser.webNavigation.onHistoryStateUpdated.addListener(function (details) {
     console.log(details);
     browser.tabs.sendMessage(details.tabId, "foo");
+}, {
+    url: [
+        {hostSuffix: ".youtube.com"},
+        {hostEquals: "youtube.com"},
+    ]
 });
