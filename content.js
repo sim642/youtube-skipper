223468d8562792d5fccd6e15f70d58aa4e3d57b5
Simmo Saan <simmo.saan@gmail.com>
2017-08-04 12:21:16 +0300

    Implement basic section skip

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
-alert("Youtube Skipper");
+function load() {
+    var video = document.querySelector("video");
+    console.log(video);
+
+    video.addEventListener("timeupdate", function () {
+        if (video.currentTime > 10 && video.currentTime < 20) {
+            video.currentTime = 20;
+        }
+    });
+}
+
+
+load();
 
 browser.runtime.onMessage.addListener(function (message) {
-    alert(message);
-    console.log(document.querySelector("video"))
+    load();
 });
