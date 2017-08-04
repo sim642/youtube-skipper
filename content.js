4f43993326ebd683e397309a4e44d0a83fa78afb
Simmo Saan <simmo.saan@gmail.com>
2017-08-04 12:37:04 +0300

    Detect video ID from URL

--- content.js
+++ content.js
@@ -13,6 +13,10 @@ function load() {
     video = document.querySelector("video");
     console.log(video);
 
+    var url = new URL(window.location.href);
+    var videoId = url.searchParams.get("v");
+    console.log(videoId);
+
     onTimeUpdate = function () {
         if (video.currentTime > 10 && video.currentTime < 20) {
             video.currentTime = 20;
